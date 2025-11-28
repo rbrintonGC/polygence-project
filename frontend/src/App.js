c12c845f46780b07e38de20aaed0308a65b3e44a
rbrintonGC <rbrinton2008@gmail.com>
2025-11-28 15:32:35 -0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
 import "./styles.css";
+import { useState } from "react";
+import Inputs from "./Inputs";
+import Return from "./Return";
 
 export default function App() {
+  const [yearlyBudget, setYearlyBudget] = useState(0);
+
   return (
-    <div className="App">
-      <h1>BUDGET YOUR LIFE</h1>
-      <h2>
-        Input monthly budgets in the boxes below to begin. Put '0' for ignored
-        fields.
-      </h2>
-      <h2>V</h2>
+    <div>
+      <div className="App">
+        <h1>BUDGET YOUR LIFE</h1>
+        <h2>
+          Input monthly budgets in the boxes below to begin. Put '0' for ignored
+          fields.
+        </h2>
+        <h2>V</h2>
+      </div>
+
+      <Inputs onSubmit={setYearlyBudget} />
+      <Return yearlyBudget={yearlyBudget} />
     </div>
   );
 }
